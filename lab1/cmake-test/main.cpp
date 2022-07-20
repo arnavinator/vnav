4822f1ada8b6f2863cf826efedfb2e9b908ed12a
cs141 <arnavsrivastava@college.harvard.edu>
2022-07-20 00:09:52 -0700

    lab 1 finished

---
+++ lab1/cmake-test/main.cpp
@@ -0,0 +1,9 @@
+#include "my_lib.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!\n";
+	cout << my_lib_func() << '\n';
+	return 0;
+}
---
+++ lab1/cmake-test/my_lib.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include "my_lib.hpp"
+
+std::string my_lib_func() {
+	return "my library's function!";
+}
+
+
